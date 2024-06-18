ee073546dc24297d6224b9d99b4d20f3042b1821
Eleonora <eleoc4@gmail.com>
2024-06-18 08:56:05 -0500

    second commit

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
+
 input_values=[1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
@@ -16,3 +18,7 @@ ax.tick_params(labelsize=14)
 
 
 plt.show()
+
+# gggg
+
+# print(plt.style.available)
